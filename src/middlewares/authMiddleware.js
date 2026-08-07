f106a36ae98380342c31dc7cef7294644676429b
zestt2024-dot <zestt2024@gmail.com>
2026-08-07 10:31:07 +0300

    update protect middleware to accept raw token without bearer

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,8 +4,8 @@ import User from '../models/User.js';
 export const protect = async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
+  if (req.headers.authorization) {
+    token = req.headers.authorization;
   }
 
   if (!token) {
